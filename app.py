5a1e2a3aed15fcef47a0a51a3cd1f3c7c1c4ebd2
Arnav Kumar <arnav9637@gmail.com>
2025-06-13 18:13:27 +0530

    feed API URL first then call the loading fucntion

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ import streamlit as st
 import requests
 import os
 import time
+BACKEND_API_URL = os.getenv("BACKEND_API_URL")
 from app_utils import wait_for_backend 
 
 st.set_page_config(page_title="Admin Dashboard - SmartDoc AI", layout="wide")
 st.title("🛠 Admin Dashboard - SmartDoc AI")
 
-BACKEND_API_URL = os.getenv("BACKEND_API_URL")
+
 
 st.info("Attempting to connect to the backend API...")
 if wait_for_backend():
